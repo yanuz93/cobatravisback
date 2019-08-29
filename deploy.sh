7e1cc74594bf5032995a8200187b1a5b348203cd
yanuz93 <yanuzpurnomo@gmail.com>
2019-08-30 06:21:17 +0700

    coba

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker stop helloworld
-docker rm helloworld
-docker rmi mathricks/flask-back:sudah
-docker run -d --name helloworld -p 8080:8080 mathricks/flask-back:sudah
+sudo docker stop helloworld
+sudo docker rm helloworld
+sudo docker rmi mathricks/flask-back:sudah
+sudo docker run -d --name helloworld -p 8080:8080 mathricks/flask-back:sudah
 
